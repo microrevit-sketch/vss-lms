b58e5ab4f27a533de1d02b91748d3e8977f3601b
saffiullahmalik <your.mmsaffiullah@gmail.com>
2025-12-16 00:31:19 +0500

    publishtry1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider, useAuth } from './contexts/AuthContext';
 import Navbar from './components/shared/Navbar';
 import Footer from './components/shared/Footer';
